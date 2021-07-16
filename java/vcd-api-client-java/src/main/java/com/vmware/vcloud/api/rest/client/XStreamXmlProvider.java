4e336255f920b26db564f93ae22c47511fe37ac5
ankitalode <alode@vmware.com>
2021-07-16 10:49:32 +0000

    SDK v10.0 release
    Signed-off-by: Ankita Lode  <alode@vmware.com>

--- java/vcd-api-client-java/src/main/java/com/vmware/vcloud/api/rest/client/XStreamXmlProvider.java
+++ java/vcd-api-client-java/src/main/java/com/vmware/vcloud/api/rest/client/XStreamXmlProvider.java
@@ -1,10 +1,33 @@
-/* **********************************************************************
- * api-extension-template-vcloud-director
- * Copyright 2018 VMware, Inc.
- * SPDX-License-Identifier: BSD-2-Clause
- * *********************************************************************/
-
 package com.vmware.vcloud.api.rest.client;
+
+/*-
+ * #%L
+ * vcd-api-client-java :: vCloud Director REST Client
+ * %%
+ * Copyright (C) 2018 - 2021 VMware
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,6 +48,7 @@ import javax.ws.rs.ext.MessageBodyWriter;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.security.AnyTypePermission;
 
 /**
  * JAX-RS's Provider to handle XStream in XML format.
@@ -36,6 +60,10 @@ public class XStreamXmlProvider implements MessageBodyReader<Object>, MessageBod
     private static final XStream xstream = new XStream();
     private static final String DEFAULT_ENCODING = "utf-8";
 
+    static {
+        XStream.setupDefaultSecurity(xstream);
+        xstream.addPermission(AnyTypePermission.ANY);
+    }
 
     @Override
     public boolean isReadable(Class<?> classType, Type genericType, Annotation[] annotations, javax.ws.rs.core.MediaType mediaType) {
@@ -87,3 +115,4 @@ public class XStreamXmlProvider implements MessageBodyReader<Object>, MessageBod
         return xstream;
     }
 }
+
